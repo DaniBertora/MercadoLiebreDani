7dd4c0429fda8edceeac4cd1ee14a83c8bddc93a
DaniBertora <danibertora@hotmail.com>
2022-05-19 19:08:10 -0300

    agregue procfile y cambie puerto ara heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ("express") ;
 const app = express() ;
 const path = require("path") ;
-app.listen(3000, () => console.log("estoy corriendo en el puerto 3000")) ;
+app.listen(process.env.PORT || 3000, () => console.log("estoy corriendo en el puerto 3000")) ;
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/" , (req, res) => {
     const laRuta = path.resolve(__dirname, "./views/home.html") ;
